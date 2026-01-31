ca0b04c94f70e65c2dbaa51aba86b48cb6692c99
Elango sakthivel <elangosakthivel0@gmail.com>
2026-01-31 23:17:09 +0530

    Add common passwords and keyboard patterns constants

---
+++ constants.py
@@ -0,0 +1,8 @@
+COMMON_PASSWORDS = {
+    "password", "123456", "123456789", "qwerty", "abc123",
+    "password1", "admin", "letmein", "welcome", "iloveyou"
+}
+
+KEYBOARD_PATTERNS = [
+    "qwerty", "asdf", "zxcv", "12345", "09876"
+]
